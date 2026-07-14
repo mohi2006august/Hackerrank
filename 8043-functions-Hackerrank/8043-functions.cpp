7430f49ced0c87c6bdb0d59cbc31cb7bc5adc87f
MOHAMMED MOHIUDDIN <nizamuddinnumaan822@gmail.com>
2026-07-15 00:52:58 +0530

    Force committed solution - Time: O(1), Space: O(1)

---
+++ 8043-functions-Hackerrank/8043-functions.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+
+int max_of_four(int a, int b, int c, int d) {
+    int max = a;
+    if (b > max) max = b;
+    if (c > max) max = c;
+    if (d > max) max = d;
+    return max;
+}
+
+int main() {
+    int a, b, c, d;
+    cin >> a >> b >> c >> d;
+    cout << max_of_four(a, b, c, d) << endl;
+    return 0;
+}
+
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
